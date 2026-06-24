426262d75d27ca59921b9e81fb2a8e0c6b1f2dad
campushoquei <jvalls.e7@gmail.com>
2026-06-24 11:23:44 +0200

    Update admin.js

--- admin.js
+++ admin.js
@@ -8,7 +8,7 @@
    SCRIPT_URL buit = MODE DEMO amb dades d'exemple generades.
    ============================================================ */
 
-const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbxBmr_cLfJ1wgpYBSxaMLwhhJyr-UXTZPPGeXBJEHrml8834c9Z6T2GC7mm5mgHMBYl/exec";
+const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbxxDapcQ_Fbwxl0ohzRN_NfQ23eMhrCpNYGibrZXMTRRS6SMqfoDYblGWJwOHFVgX6ibg/exec";
 
 const PIN_KEY = "casal_admin_pin";
 const EXP_KEY = "casal_admin_exp";      // caducitat de sessió (timestamp)
